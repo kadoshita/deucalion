b2e14dd78549e520e7019758a7ca0fce1645155c
kadoshita <sublimer.me@gmail.com>
2021-08-22 13:03:19 +0900

    Update class slack transmitter

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,14 @@
 import { slackMessageGenerator } from '../generator';
 import { Message } from '../message/messageInterface';
+import { slackTransmitter } from '../transmitter';
 
 export const handler = async (message: Message): Promise<void> => {
     switch (message.type) {
-        case 'prometheus':
-            await slackMessageGenerator.handle(message);
+        case 'prometheus': {
+            const slackMessage = await slackMessageGenerator.handle(message);
+            if (slackMessage) {
+                await slackTransmitter.send(slackMessage);
+            }
+        }
     }
 };
