7ebfe1af55bbb20e51775bc591ffcdc4a78153cf
kadoshita <sublimer.me@gmail.com>
2021-08-23 20:58:56 +0900

    Add grafana graph image upload to oci object storage

--- src/external/grafana.ts
+++ src/external/grafana.ts
@@ -1,12 +1,41 @@
-import { S3_ENDPOINT_URL } from '../config';
+import fetch from 'node-fetch';
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+import { GRAFANA_API_KEY, OCI_ACCESS_KEY, OCI_OBJECT_STORAGE_BUCKET, OCI_OBJECT_STORAGE_NAMESPACE, OCI_OBJECT_STORAGE_REGION, OCI_OBJECT_STORAGE_URL, OCI_SECRET_KEY } from '../config';
+
+const client = new S3Client({
+    region: OCI_OBJECT_STORAGE_REGION,
+    endpoint: `https://${OCI_OBJECT_STORAGE_NAMESPACE}.compat.objectstorage.${OCI_OBJECT_STORAGE_REGION}.oraclecloud.com`,
+    credentials: {
+        accessKeyId: OCI_ACCESS_KEY,
+        secretAccessKey: OCI_SECRET_KEY
+    },
+    forcePathStyle: true,
+});
 
 export interface GetGraphImageURLParams {
-    start_at: Date;
-    end_at: Date;
-    metrics: 'cpu' | 'memory',
+    url: string;
+    panelId: string;
+    startAt: Date;
+    endAt: Date;
     instance: string;
 }
 export const getGraphImageURL = async (params: GetGraphImageURLParams): Promise<string> => {
-    // return `https://${S3_ENDPOINT_URL}/grafana-image/${params.instance}/${params.metrics}/1623498095000/900`;
-    return 'https://media.sublimer.me/grafana-image/home.sublimer.me:9104/cpu/1623498095000/900';
+    const from = params.startAt.getTime();
+    const to = params.endAt.getTime();
+    const grafanaGraphImgeURL = `${params.url}&var-hosts=${params.instance}&from=${from}&to=${to}&panelId=${params.panelId}&width=1000&height=500&tz=Asia%2FTokyo`;
+    
+    const res = await fetch(grafanaGraphImgeURL, {
+        headers: {
+            'Authorization': `Bearer ${GRAFANA_API_KEY}`
+        }
+    });
+    const data = await res.buffer();
+    const objectKey = `deucalion_graph_image/${params.instance}/${params.panelId}/${from}/${to}/graph`;
+    const command = new PutObjectCommand({
+        Bucket: OCI_OBJECT_STORAGE_BUCKET,
+        Body: data,
+        Key: objectKey
+    });
+    await client.send(command);
+    return `${OCI_OBJECT_STORAGE_URL}${objectKey}`;
 };
