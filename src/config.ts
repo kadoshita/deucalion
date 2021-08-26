aa5aef78a39716a4f34b4815d574f6d01aeb97de
kadoshita <sublimer.me@gmail.com>
2021-08-26 20:49:03 +0900

    Add prometheus query

--- src/config.ts
+++ src/config.ts
@@ -5,6 +5,7 @@ dotenv.config();
 export const SLACK_API_TOKEN = process.env.SLACK_API_TOKEN || '';
 export const SLACK_POST_CHANNEL_ID = process.env.SLACK_POST_CHANNEL_ID || '';
 export const GRAFANA_API_KEY = process.env.GRAFANA_API_KEY || '';
+export const PROMETHEUS_ENDPOINT = process.env.PROMETHEUS_ENDPOINT || '';
 export const OCI_ACCESS_KEY = process.env.OCI_ACCESS_KEY || '';
 export const OCI_SECRET_KEY = process.env.OCI_SECRET_KEY || '';
 export const OCI_OBJECT_STORAGE_URL = process.env.OCI_OBJECT_STORAGE_URL || '';
--- src/external/prometheus.ts
+++ src/external/prometheus.ts
@@ -1,4 +1,11 @@
+import fetch from 'node-fetch';
+import { PROMETHEUS_ENDPOINT } from '../config';
 
 export const query = async (query: string): Promise<any> => {
-    return 100;
+    const res = await fetch(`${PROMETHEUS_ENDPOINT}/api/v1/query?query=${query}`).then(res => res.json());
+    if (res.data.result.length === 0) {
+        return;
+    }
+    const [_, result] = res.data.result[0].value;
+    return result;
 };
