e52f8fa7eb0bde1367ddf3cfa3a373216aba8393
kadoshita <sublimer.me@gmail.com>
2021-09-20 18:06:36 +0900

    Update slack message format

--- src/message/slack.ts
+++ src/message/slack.ts
@@ -1,24 +1,14 @@
-import { MessageAttachment } from '@slack/types';
+import { Block, MessageAttachment } from '@slack/types';
 import { randomUUID } from 'crypto';
 import { Message, MessageType } from './messageInterface';
 
 export type SlackAttachmentColor = '#BE281B' | '#D1A14D' | '#439FE0' | '#359C4C';
-export interface SlackMessageAttachment {
-    color: SlackAttachmentColor;
-    blocks: MessageAttachment;
-}
-
 export class SlackMessage implements Message {
     readonly id: string;
     readonly type: MessageType;
-    readonly message: SlackMessageAttachment[];
 
-    constructor(color: SlackAttachmentColor, blocks: MessageAttachment) {
+    constructor(readonly attachments?: MessageAttachment[], readonly blocks?: Block[]) {
         this.id = randomUUID();
         this.type = 'slack';
-        this.message = [{
-            color,
-            blocks
-        }];
     }
 }
