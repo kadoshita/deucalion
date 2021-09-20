e46cd4542ab4ab4835726061ef414fb2e29ad11b
kadoshita <sublimer.me@gmail.com>
2021-09-20 18:06:14 +0900

    Update mock for test

--- __tests__/utils/slackTransmitterSendMock.ts
+++ __tests__/utils/slackTransmitterSendMock.ts
@@ -2,10 +2,10 @@ import { SlackMessage } from '../../src/message/slack';
 import { slackTransmitter } from '../../src/transmitter';
 
 let slackTransmitterSendMock: jest.SpyInstance;
-
+jest.mock('@slack/web-api');
 beforeEach(() => {
     slackTransmitterSendMock = jest.spyOn(slackTransmitter, 'send');
-    slackTransmitterSendMock.mockImplementation(async (message: SlackMessage): Promise<void> => {
+    slackTransmitterSendMock.mockImplementation(async (messages: SlackMessage[]): Promise<void> => {
         return;
     });
 });
