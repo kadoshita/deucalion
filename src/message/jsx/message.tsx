44a5275ed2e332e61a27571ae88ad5dfbe5102c7
kadoshita <sublimer.me@gmail.com>
2021-09-20 18:07:04 +0900

    Update slack block format

--- src/message/jsx/message.tsx
+++ src/message/jsx/message.tsx
@@ -1,54 +1,14 @@
-import { MessageAttachment } from '@slack/web-api';
-import JSXSlack, {
-    Blocks,
-    Divider,
-    Field,
-    Header,
-    Image,
-    Section,
-} from 'jsx-slack';
+import { Block } from '@slack/web-api';
+import JSXSlack, { Blocks, Image } from 'jsx-slack';
 
-export interface SlackAlertMessageProps {
-    title: string;
-    description: string;
+export interface SlackBlockMessageProps {
     image_url: string;
-    current_value: string;
-    severity: string;
-    start_at: string;
-    end_at: string;
 }
-export const SlackAlertMessage = ({
-    title,
-    description,
+export const createSlackBlockMessage = ({
     image_url,
-    current_value,
-    severity,
-    start_at,
-    end_at,
-}: SlackAlertMessageProps): MessageAttachment =>
+}: SlackBlockMessageProps): Block[] =>
     JSXSlack(
         <Blocks>
-            <Header>{title}</Header>
-            <Section>{description}</Section>
             <Image src={image_url} alt="graph"></Image>
-            <Section>
-                <Field>Current Value</Field>
-                <Field>{current_value}</Field>
-            </Section>
-            <Divider></Divider>
-            <Section>
-                <Field>Severity</Field>
-                <Field>{severity}</Field>
-            </Section>
-            <Divider></Divider>
-            <Section>
-                <Field>Start At</Field>
-                <Field>{start_at}</Field>
-            </Section>
-            <Divider></Divider>
-            <Section>
-                <Field>End At</Field>
-                <Field>{end_at}</Field>
-            </Section>
         </Blocks>
-    ) as MessageAttachment;
+    ) as Block[];
